0955cea013ad5fdcc1d3b0d4a786b813a6ddda7e
beykery <874343737@qq.com>
2014-03-16 14:12:54 +0800

    添加一个server部分的实现

---
+++ cocosocket-client/SocketListerner.cpp
@@ -0,0 +1,68 @@
+﻿/* 
+ * File:   SocketListerner.cpp
+ * Author: beykery
+ * 
+ * Created on 2013年12月30日, 下午4:00
+ */
+
+#include "SocketListerner.h"
+
+#ifdef WIN32
+#pragma comment(lib, "wsock32")
+#pragma comment(lib,"ws2_32.lib")
+#include <winsock2.h>
+#else
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#endif
+#include "Socket.h"
+#include "ByteBuf.h"
+#include "Protocal.h"
+
+SocketListerner::SocketListerner():context(NULL)
+{
+}
+
+SocketListerner::~SocketListerner()
+{
+}
+
+void SocketListerner::SetContext(Socket* context)
+{
+    this->context = context;
+}
+
+void SocketListerner::Run()
+{
+    ByteBuf* buf = new ByteBuf(1024);
+    int size = 0;
+    int sockid = context->GetSocketId();
+    while (true)
+    {
+        size = recv(sockid, buf->GetRaw(), 1024, 0);
+        if (size > 0)
+        {
+            buf->ReaderIndex(0);
+            buf->WriterIndex(size);
+            while (true)
+            {
+                ByteBuf* frame = context->GetProtocal()->TranslateFrame(buf);
+                if (frame != NULL)
+                {
+                    this->OnMessage(context, frame);
+                } else
+                {
+                    break;
+                }
+            }
+        } else
+        {
+            break;
+        }
+    }
+    delete buf;
+    this->OnClose(context, true);
+}
+
